0db6d011a8a50fda240abe9c0cee3f9e466757a9
GJLMoTea <gjlmotea@gmail.com>
2022-04-24 08:14:32 +0800

    place order v1

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,190 +1,199 @@
 import { Component, OnInit } from '@angular/core'
-import { WalletName, WalletReadyState} from '@solana/wallet-adapter-base'
+import { WalletName, WalletReadyState } from '@solana/wallet-adapter-base'
 import { ConnectionStore, WalletStore } from '@heavy-duty/wallet-adapter'
 import { concatMap, defer, first, from, map, Observable, throwError } from 'rxjs'
-import { PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
-import { isNotNull } from "../operators";
+import { Keypair, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import { isNotNull } from '../operators';
+import { initializeContext } from 'MamaM/sdk'
 import base58 from 'bs58';
 
 @Component({
-  selector: 'app-example',
-  templateUrl: './example.component.html',
-  styleUrls: ['./example.component.css']
+    selector: 'app-example',
+    templateUrl: './example.component.html',
+    styleUrls: ['./example.component.css']
 })
 export class ExampleComponent implements OnInit {
-  readonly connection$ = this._connectionStore.connection$;
-  readonly wallets$ = this._walletStore.wallets$;
-  readonly wallet$ = this._walletStore.wallet$;
-  readonly walletName$ = this.wallet$.pipe(
-      map((wallet) => wallet?.adapter.name || null)
-  );
-  readonly ready$ = this.wallet$.pipe(
-      map(
-          (wallet) =>
-              wallet &&
-              (wallet.adapter.readyState === WalletReadyState.Installed ||
-                  wallet.adapter.readyState === WalletReadyState.Loadable)
-      )
-  );
-  readonly connected$ = this._walletStore.connected$;
-  readonly publicKey$ = this._walletStore.publicKey$;
-  public anchorWallet$ = this._walletStore.anchorWallet$;
-  public anchorWallet: {
-    publicKey: PublicKey;
-    signTransaction: (transaction: Transaction) => Promise<Transaction>;
-    signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>;
-  } | undefined ;
-
-  lamports = 0;
-  recipient = '';
-
-  constructor(
-      private readonly _connectionStore: ConnectionStore,
-      private readonly _walletStore: WalletStore
-  ) {}
-
-  onConnect() {
-    this._walletStore.connect().subscribe();
-  }
-
-  onDisconnect() {
-    this._walletStore.disconnect().subscribe();
-  }
-
-  onSelectWallet(walletName: WalletName) {
-    this._walletStore.selectWallet(walletName);
-    this.anchorWallet$.subscribe(wallet => {
-      this.anchorWallet = wallet;
-    })
-  }
-
-  onSendTransaction(fromPubkey: PublicKey) {
-    this.connection$
-        .pipe(
-            first(),
-            isNotNull,
-            concatMap((connection) =>
-                from(defer(() => connection.getRecentBlockhash())).pipe(
-                    concatMap(({ blockhash }) =>
-                        this._walletStore.sendTransaction(
-                            new Transaction({
-                              recentBlockhash: blockhash,
-                              feePayer: fromPubkey,
-                            }).add(
-                                SystemProgram.transfer({
-                                  fromPubkey,
-                                  toPubkey: new PublicKey(this.recipient),
-                                  lamports: this.lamports,
-                                })
-                            ),
-                            connection
-                        )
-                    )
-                )
-            )
+    readonly connection$ = this._connectionStore.connection$;
+    readonly wallets$ = this._walletStore.wallets$;
+    readonly wallet$ = this._walletStore.wallet$;
+    readonly walletName$ = this.wallet$.pipe(
+        map((wallet) => wallet?.adapter.name || null)
+    );
+    readonly ready$ = this.wallet$.pipe(
+        map(
+            (wallet) =>
+                wallet &&
+                (wallet.adapter.readyState === WalletReadyState.Installed ||
+                    wallet.adapter.readyState === WalletReadyState.Loadable)
         )
-        .subscribe(
-            (signature) => console.log(`Transaction sent (${signature})`),
-            (error) => console.error(error)
-        );
-  }
+    );
+    readonly connected$ = this._walletStore.connected$;
+    readonly publicKey$ = this._walletStore.publicKey$;
+    public anchorWallet$ = this._walletStore.anchorWallet$;
+    public anchorWallet: {
+        publicKey: PublicKey;
+        signTransaction: (transaction: Transaction) => Promise<Transaction>;
+        signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>;
+    } | undefined;
 
-  onSignTransaction(fromPubkey: PublicKey) {
-    this.connection$
-        .pipe(
-            first(),
-            isNotNull,
-            concatMap((connection) =>
-                from(defer(() => connection.getRecentBlockhash())).pipe(
-                    map(({ blockhash }) =>
-                        new Transaction({
-                          recentBlockhash: blockhash,
-                          feePayer: fromPubkey,
-                        }).add(
-                            SystemProgram.transfer({
-                              fromPubkey,
-                              toPubkey: new PublicKey(this.recipient),
-                              lamports: this.lamports,
-                            })
-                        )
-                    )
-                )
-            ),
-            concatMap((transaction) => {
-              const signTransaction$ =
-                  this._walletStore.signTransaction(transaction);
+    lamports = 0;
+    recipient = '';
 
-              if (!signTransaction$) {
-                return throwError(
-                    new Error('Sign transaction method is not defined')
-                );
-              }
+    constructor(
+        private readonly _connectionStore: ConnectionStore,
+        private readonly _walletStore: WalletStore
+    ) {
+    }
 
-              return signTransaction$;
-            })
-        )
-        .subscribe(
-            (transaction) => console.log('Transaction signed', transaction),
-            (error) => console.error(error)
-        );
-  }
+    onConnect() {
+        this._walletStore.connect().subscribe();
+    }
 
-  onSignAllTransactions(fromPubkey: PublicKey) {
-    this.connection$
-        .pipe(
-            first(),
-            isNotNull,
-            concatMap((connection) =>
-                from(defer(() => connection.getRecentBlockhash())).pipe(
-                    map(({ blockhash }) =>
-                        new Array(3).fill(0).map(() =>
-                            new Transaction({
-                              recentBlockhash: blockhash,
-                              feePayer: fromPubkey,
-                            }).add(
-                                SystemProgram.transfer({
-                                  fromPubkey,
-                                  toPubkey: new PublicKey(this.recipient),
-                                  lamports: this.lamports,
-                                })
+    onDisconnect() {
+        this._walletStore.disconnect().subscribe();
+    }
+
+    onSelectWallet(walletName: WalletName) {
+        this._walletStore.selectWallet(walletName);
+        let keypair: Keypair;
+        this.anchorWallet$.subscribe(wallet => {
+            this.anchorWallet = wallet;
+            if (wallet) {
+                let w = initializeContext(wallet);
+                w.then(); // TODO
+            } else {
+                console.warn('wallet undefined!!');
+            }
+        })
+    }
+
+    onSendTransaction(fromPubkey: PublicKey) {
+        this.connection$
+            .pipe(
+                first(),
+                isNotNull,
+                concatMap((connection) =>
+                    from(defer(() => connection.getRecentBlockhash())).pipe(
+                        concatMap(({blockhash}) =>
+                            this._walletStore.sendTransaction(
+                                new Transaction({
+                                    recentBlockhash: blockhash,
+                                    feePayer: fromPubkey,
+                                }).add(
+                                    SystemProgram.transfer({
+                                        fromPubkey,
+                                        toPubkey: new PublicKey(this.recipient),
+                                        lamports: this.lamports,
+                                    })
+                                ),
+                                connection
                             )
                         )
                     )
                 )
-            ),
-            concatMap((transactions) => {
-              const signAllTransaction$ =
-                  this._walletStore.signAllTransactions(transactions);
-
-              if (!signAllTransaction$) {
-                return throwError(
-                    new Error('Sign all transactions method is not defined')
-                );
-              }
-
-              return signAllTransaction$;
-            })
-        )
-        .subscribe(
-            (transactions) => console.log('Transactions signed', transactions),
-            (error) => console.error(error)
-        );
-  }
-
-  onSignMessage() {
-    const signMessage$ = this._walletStore.signMessage(
-        new TextEncoder().encode('Hello world!')
-    );
-
-    if (!signMessage$) {
-      return console.error(new Error('Sign message method is not defined'));
+            )
+            .subscribe(
+                (signature) => console.log(`Transaction sent (${signature})`),
+                (error) => console.error(error)
+            );
     }
 
-    signMessage$.pipe(first()).subscribe((signature) => {
-      console.log(`Message signature: ${base58.encode(signature)}`);
-    });
-  }
+    onSignTransaction(fromPubkey: PublicKey) {
+        this.connection$
+            .pipe(
+                first(),
+                isNotNull,
+                concatMap((connection) =>
+                    from(defer(() => connection.getRecentBlockhash())).pipe(
+                        map(({blockhash}) =>
+                            new Transaction({
+                                recentBlockhash: blockhash,
+                                feePayer: fromPubkey,
+                            }).add(
+                                SystemProgram.transfer({
+                                    fromPubkey,
+                                    toPubkey: new PublicKey(this.recipient),
+                                    lamports: this.lamports,
+                                })
+                            )
+                        )
+                    )
+                ),
+                concatMap((transaction) => {
+                    const signTransaction$ =
+                        this._walletStore.signTransaction(transaction);
 
-  ngOnInit(): void {
-  }
+                    if (!signTransaction$) {
+                        return throwError(
+                            new Error('Sign transaction method is not defined')
+                        );
+                    }
+
+                    return signTransaction$;
+                })
+            )
+            .subscribe(
+                (transaction) => console.log('Transaction signed', transaction),
+                (error) => console.error(error)
+            );
+    }
+
+    onSignAllTransactions(fromPubkey: PublicKey) {
+        this.connection$
+            .pipe(
+                first(),
+                isNotNull,
+                concatMap((connection) =>
+                    from(defer(() => connection.getRecentBlockhash())).pipe(
+                        map(({blockhash}) =>
+                            new Array(3).fill(0).map(() =>
+                                new Transaction({
+                                    recentBlockhash: blockhash,
+                                    feePayer: fromPubkey,
+                                }).add(
+                                    SystemProgram.transfer({
+                                        fromPubkey,
+                                        toPubkey: new PublicKey(this.recipient),
+                                        lamports: this.lamports,
+                                    })
+                                )
+                            )
+                        )
+                    )
+                ),
+                concatMap((transactions) => {
+                    const signAllTransaction$ =
+                        this._walletStore.signAllTransactions(transactions);
+
+                    if (!signAllTransaction$) {
+                        return throwError(
+                            new Error('Sign all transactions method is not defined')
+                        );
+                    }
+
+                    return signAllTransaction$;
+                })
+            )
+            .subscribe(
+                (transactions) => console.log('Transactions signed', transactions),
+                (error) => console.error(error)
+            );
+    }
+
+    onSignMessage() {
+        const signMessage$ = this._walletStore.signMessage(
+            new TextEncoder().encode('Hello world!')
+        );
+
+        if (!signMessage$) {
+            return console.error(new Error('Sign message method is not defined'));
+        }
+
+        signMessage$.pipe(first()).subscribe((signature) => {
+            console.log(`Message signature: ${base58.encode(signature)}`);
+        });
+    }
+
+    ngOnInit(): void {
+    }
 }
--- src/app/order-book/order-book.component.ts
+++ src/app/order-book/order-book.component.ts
@@ -1,29 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-order-book',
-  templateUrl: './order-book.component.html',
-  styleUrls: ['./order-book.component.css']
+    selector: 'app-order-book',
+    templateUrl: './order-book.component.html',
+    styleUrls: ['./order-book.component.css']
 })
 export class OrderBookComponent implements OnInit {
 
-  public bidsList = [
-    [75.8754, 10],
-    [50.0655, 8],
-    [24.0614, 9],
-  ];
-  public asksList = [
-    [101.5473, 11],
-    [126.9725, 7],
-    [152.2083, 9],
-    [177.2546, 3],
-    [202.1187, 6],
-  ];
+    public mode = -1; // -1: Default Value, 0: Sell, 1: Buy
+    public price = 0;
+    public amount = 0;
 
-  constructor() { }
+    public bidsList = [
+        [75.8754, 10],
+        [50.0655, 8],
+        [24.0614, 9],
+    ];
+    public asksList = [
+        [101.5473, 11],
+        [126.9725, 7],
+        [152.2083, 9],
+        [177.2546, 3],
+        [202.1187, 6],
+    ];
 
-  ngOnInit(): void {
-  //  每五秒呼叫一次
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        //  每五秒呼叫一次更新報價
+    }
+
+    public changeMode(mode: number) {
+        console.log(mode, 'changeMode')
+        this.mode = mode;
+    }
+
+    public submit() {
+        console.log('submit ', 'price:', this.price, 'amount:', this.amount, 'mode:', this.mode)
+        if (this.price <= 0 || this.amount <= 0 || this.mode === -1){
+            // 不給submit
+        }
+        // TODO
+    }
 
 }
